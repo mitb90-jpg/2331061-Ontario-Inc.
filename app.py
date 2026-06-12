2a928f5526655ddb4f94882e0b0aa51b9c9a1e9e
mitb90-jpg <mitb90@gmail.com>
2026-06-12 17:39:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(
 col1, col2 = st.columns([1, 4])
 
 with col1:
-    st.image("logo.png", width=140)
+st.image("logo.jpeg", width=140)
 
 with col2:
     st.title("🏢 Prime Accounting and Tax")
